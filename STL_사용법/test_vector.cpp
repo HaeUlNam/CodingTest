466bc2b4ee8504858368ba94e755030f6aeedbe4
HaeUlNam <harrynam3@naver.com>
2020-04-05 21:37:20 +0900

    Update test_vector.cpp

--- STL_사용법/test_vector.cpp
+++ STL_사용법/test_vector.cpp
@@ -1,2 +1,7 @@
-//TODO: emplace_back과 push_back 성능 비교
-//출처: https://openmynotepad.tistory.com/10
+/*
+TODO1: emplace_back과 push_back 성능 비교
+출처: https://openmynotepad.tistory.com/10
+
+TODO2: front(), back()
+
+*/
