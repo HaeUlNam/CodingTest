adaec25ddd63c5bcd24862338b6fbb07d4b2ff47
HaeUlNam <harrynam3@naver.com>
2020-04-05 23:43:36 +0900

    Update test_vector.cpp

--- STL_사용법/test_vector.cpp
+++ STL_사용법/test_vector.cpp
@@ -1,7 +1,9 @@
 /*
 TODO1: emplace_back과 push_back 성능 비교
-출처: https://openmynotepad.tistory.com/10
+ㄴ출처: https://openmynotepad.tistory.com/10
 
 TODO2: front(), back()
 
+TODO3: sort(rbegin(), rend())...
+ㄴ출처: https://stackoverflow.com/questions/9025084/sorting-a-vector-in-descending-order
 */
